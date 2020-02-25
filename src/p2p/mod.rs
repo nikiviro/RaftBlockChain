3ea058de132e5315be8ee09df674b9bf733e6706
Nikolas Virostek <virostek@metaapp.sk>
2020-02-25 23:46:11 +0100

    separate peer to module

---
+++ src/p2p/mod.rs
@@ -0,0 +1,3 @@
+pub mod peer;
+pub mod peers;
+pub mod networkManager;
---
+++ src/p2p/peer.rs
@@ -0,0 +1,25 @@
+
+use zmq::{Socket, Context};
+
+
+pub struct Peer {
+    pub peer_port: u64,
+    pub socket: Socket,
+}
+
+impl Peer {
+    pub fn new(
+        peer_port: u64,
+        context: Context
+    ) -> Self {
+        let dealer_socket = context.socket(zmq::DEALER).unwrap();
+        dealer_socket
+            .connect(format!("tcp://localhost:{}", peer_port.to_string()).as_ref())
+            .expect("Failed to connect to peer");
+
+        Peer {
+            peer_port: peer_port,
+            socket: dealer_socket,
+        }
+    }
+}
